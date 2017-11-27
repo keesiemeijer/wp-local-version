6c5fd9e699be8b2e16efeb08e733542e7bac2a65
Kees Meijer <keesie.meijer@gmail.com>
2017-11-27 15:15:13 +0100

    Update install information

--- wp-local-version.sh
+++ wp-local-version.sh
@@ -2,71 +2,77 @@
 
 # =============================================================================
 # WP Local Version
-# 
+#
 # Version 2.0.0
 #
 # By keesiemeijer
 # https://github.com/keesiemeijer/wp-local-version
 #
-# This bash script lets you install any WordPress version in the Local by Freewheel app without getting PHP errors.
-# 
+# This bash script lets you install any WordPress version in the Local by Freewheel app (without getting PHP errors).
+#
 # ********* Features *********
-# 
+#
 #     WP-CLI is used to install WP versions > 3.5.2
 #     Older WP versions are installed by this script.
 #     Ability to keep the wp-content folder between versions.
-# 
+#
 # ********* PHP Version *********
-# 
+#
 # WordPress is not compatible with all PHP versions (in the Local app).
-# Here's an overview of what PHP version you'll have use for WordPress versions.
+# Here's an overview of what PHP version you'll have to use for WordPress to be installed successfully.
 #     WordPress < 4.7 needs PHP 5.6 or lower (or you get a warning)
 #     WordPress < 3.9 needs PHP 5.3 or lower (or you get a fatal error)
-# 
+#
 # ********* Requirements *********
-# 
+#
 #     rsync
-# 
+#
 # To sync the wp-content folder between installations rsync is required..
 # If it's not installed already, right click the site and choose Open Site SSH.
 # Go to the /app folder
 #  	cd /app
-# 	
+#
 # Install rsync
 #   apt-get install -y rsync
-#  
+#
 # ********* Warning !!! *********
-# 	
+#
 # The database and directory for the WordPress install (except wp-content) are deleted prior to installing.
 #     Back up files outside wp-content (if you don't want them deleted) before installing a new version.
-# 
+#
 # This script fixes (fatal) errors for earlier versions (WP < 2.0) by hacking core files.
 # This script hides errors by setting error_reporting off in:
 #     wp-config.php (WP < 3.5.2)
 #     wp-settings.php (WP < 3.0.0)
-#     
+#
 # ********* Installation *********
 # To install this script go to the website's /app folder
-# 	cd path/to/local/website/app
-# 		
-# 
+#     cd path/to/local/website/app
+#
+# And download this script
+#     curl -o wp-local-version.sh https://raw.githubusercontent.com/keesiemeijer/wp-local-version/master/wp-local-version.sh
+#
+# Finally, edit the variables in this script to match your site.
+#
+# **Note** It's important to edit the DOMAIN variable 
+#          otherwise you can't visit the site after installing a new WP version. 
 #
 # ********* Installing a new WP Version *********
 # Please read the Warning section above before installing new WP versions.
-# 
+#
 # To install a new WP version follow the following steps
 #     1 Right click the site and choose Open Site SSH.
 #     2 Go to the /app folder
 #         cd /app
 #     3 Install a new WP version. (Without a version number the latest WP version is installed)
 #         bash wp-local-version.sh 4.4
-#         
+#
 # If all went well it shows a message with instructions how to finish the install.
-# 
+#
 # ********* License *********
-# 
+#
 # License: GPL-2.0+
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
@@ -82,27 +88,29 @@
 
 # =============================================================================
 # Variables
-# 
+#
 # Note: Don't use spaces around the equal sign when editing variables below.
 # =============================================================================
 
 # Domain name
-# Note: If edited, you'll need to edit it in the vvv-hosts and the vvv-nginx.conf files as well.
-readonly HOME_URL="suprissse.local"
+readonly DOMAIN="yourwebsite.local"
 
 
-# WordPress version to be installed. Default: "0.71-gold"
+# WordPress version to be installed. Default: latest version
 # See the release archive: https://wordpress.org/download/release-archive/
-# 
+#
 # Use a version number or "latest"
 WP_VERSION="latest"
 
 # Remove errors. Default true
 readonly REMOVE_ERRORS=false
+
+# Keep the current wp-content folder for this website
+# If set to false you loose everything you changed in the wp-content folder
 readonly KEEP_WP_CONTENT=false
 
 # Database credentials
-readonly DB_NAME="wp-nostalgia"
+readonly DB_NAME="wp-local-version"
 readonly DB_USER="wp"
 readonly DB_PASS="wp"
 
@@ -110,12 +118,10 @@ readonly DB_PASS="wp"
 readonly WP_USER="admin"
 readonly WP_PASS="password"
 
-
-
 # =============================================================================
-# 
+#
 # That's all, stop editing!
-# 
+#
 # =============================================================================
 
 # current path
@@ -141,7 +147,7 @@ function is_dir() {
 	[[ -d $dir ]]
 }
 
-printf "\nStart Setup '%s'...\n" "$HOME_URL"
+printf "\nStart Setup '%s'...\n" "$DOMAIN"
 
 if [[ "$KEEP_WP_CONTENT" = true ]]; then
 
@@ -227,13 +233,13 @@ config_error=$(wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_P
 if [[ ! "$config_error" ]]; then
 	# WP >= 3.5.2
 
-	wp core install --url="$HOME_URL" --title="$TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASS" --admin_email=demo@example.com --allow-root
-	finished="Visit $HOME_URL/wp-admin Username: admin, Password: password"
+	wp core install --url="$DOMAIN" --title="$TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASS" --admin_email=demo@example.com --allow-root
+	finished="Visit $DOMAIN/wp-admin Username: admin, Password: password"
 else
 	# WP < 3.5.2
 
 	config_file="wp-config-sample.php"
-	finished="Visit $HOME_URL/readme.html and follow the install instructions"
+	finished="Visit $DOMAIN/readme.html and follow the install instructions"
 
 	if is_file "$INSTALL_PATH/$config_file"; then
 
@@ -261,7 +267,7 @@ else
 	if is_file "$INSTALL_PATH/b2config.php"; then
 
 		config_file="b2config.php"
-		sed -i -e "s/http:\/\/example.com/http:\/\/$HOME_URL/g" "$INSTALL_PATH/$config_file"
+		sed -i -e "s/http:\/\/example.com/http:\/\/$DOMAIN/g" "$INSTALL_PATH/$config_file"
 
 		if [[ "$REMOVE_ERRORS" = true ]]; then
 			printf "\n<?php error_reporting( 0 ); ?>\n" >> "$INSTALL_PATH/$config_file"
@@ -336,6 +342,6 @@ if [[ "$REMOVE_ERRORS" = true && "$config_error" ]]; then
 	fi
 fi
 
-printf "\nFinished Setup %s with version: %s!\n" "$HOME_URL" "$WP_VERSION"
+printf "\nFinished Setup %s with version: %s!\n" "$DOMAIN" "$WP_VERSION"
 echo "$finished"
 echo ""
